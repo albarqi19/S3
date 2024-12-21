20ff1f32570f991a7b1b25fe741490f436b50610
albarqi19 <89564646+albarqi19@users.noreply.github.com>
2024-12-21 19:40:55 +0300

    Update configuration to use direct keys

--- server/config/sheets.js
+++ server/config/sheets.js
@@ -1,15 +1,12 @@
 import { google } from 'googleapis';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const auth = new google.auth.GoogleAuth({
   credentials: {
-    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL || '',
-    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n') || ''
+    client_email: "kedma-439@sonic-momentum-292409.iam.gserviceaccount.com",
+    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCz4MeFaHIpDGZl\nVBwPeehblFoCz2fyRzOO9v6nHVPioy2wGI1/h0tsDuicCNLXxwhtITZ8mdaSOULn\nyEKh38fhc81dYjfv9LQfWmaXDgVWcttAXAldxRCDg8Aj6YIew7BsRm51GnXCgkGb\nNc419UvzhiBtxeTrYhn/LmyZ7pbx/M1GAaAYphRoxqk90Ki6VMXGqcLpS8sKRBTa\nrmSxkF0ZeOuubhoO8wcD1ITtKygvpKRCjKOrcvn5"
   },
   scopes: ['https://www.googleapis.com/auth/spreadsheets']
 });
 
 export const sheets = google.sheets({ version: 'v4', auth });
-export const spreadsheetId = process.env.GOOGLE_SHEETS_ID;
+export const spreadsheetId = "1JVHUXf23kQ0ZVu8Hc1g-sqrMCUwHufw4Bj4KKGyd_j4";
